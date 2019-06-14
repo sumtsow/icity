4f209bab7aed1d7f111dadac1c035a2e52238ebc
Dmytro Sumtsov <sumtsow@gmail.com>
2019-06-14 15:11:04 +0300

    Update

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\User;
+use App\Cart;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -64,11 +65,15 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'lastname' => $data['lastname'],
             'firstname' => $data['firstname'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+        $cart = new Cart();
+        $cart->id_user = $user->id;
+        $cart->save();
+        return $user;
     }
 }
