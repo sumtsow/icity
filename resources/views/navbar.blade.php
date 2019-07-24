e5088aee74966854a0e446282c600e5e18b8a5f0
Dmytro Sumtsov <sumtsow@gmail.com>
2019-07-24 22:02:24 +0300

    Update

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,8 +1,4 @@
-<div class="col-8">
-    
-    <div class="row">
-        
-        <nav class="col-5 navbar navbar-dark p-0" style="min-height:  88px">
+        <nav class="col-1 navbar navbar-dark p-0" style="min-height:  88px">
             
             <button class="navbar-toggler" type="button" data-toggle="dropdown" aria-haspopup="true" aria-label="Toggle navigation" id="navbarDropdown">
                 <span class="navbar-toggler-icon"></span>
@@ -26,27 +22,31 @@
 
             </div>
 
-            <a class="navbar-brand text-center text-uppercase d-none d-xs-none d-sm-none d-md-block d-lg-block d-xl-block" href="/">
+        </nav>
+
+        <div class="col-2">
+            <a class="navbar-brand text-center text-uppercase d-none d-xs-none d-sm-none  d-md-block d-lg-block d-xl-block" href="/">
                 <span class="h1 font-weight-bold text-light">iCity</span><br>
                 <span class="h6 text-dark font-weight-bold"><sup>{{ __('app.online city') }}</sup></span>
             </a>
+        </div>
 
-            <a class="mx-3 mt-0 d-none d-xs-none d-sm-none d-md-none d-lg-block d-xl-block" href="#"><img src="/img/map.png" alt="map" /></a>
+        <div class="col d-flex my-auto mx-3">
+            <a class="d-none d-xs-none d-sm-none d-md-flex d-lg-flex d-xl-flex" href="#"><img src="/img/map.png" alt="Map" /></a>
+        </div>
 
-        </nav>
-
-        <div class="col-6 d-none d-xs-none d-sm-none d-md-block d-lg-block d-xl-block">
+        <div class="col d-none d-xs-none d-sm-none d-md-flex d-lg-flex d-xl-flex">
             
             <form class="form my-4 w-100">   
                 
-                <div class="row w-100 justify-content-end my-auto align-items-center">
+                <div class="d-flex justify-content-end my-auto">
 
-                    <div class="col-8">
+                    <div class="mr-2">
                         <input class="form-control mr-2" type="search" placeholder="{{ __('app.find service') }}" aria-label="{{ __('app.search') }}">
                     </div>
                     
-                    <div class="col-2">
-                        <button title="{{ __('app.search') }}" class="btn btn-outline-light my-2 my-sm-0" type="submit"><i class="fas fa-search"></i></button>
+                    <div class="mr-2">
+                        <button title="{{ __('app.search') }}" class="btn btn-outline-light" type="submit"><i class="fas fa-search"></i></button>
                     </div>
                 
                 </div>
@@ -54,26 +54,20 @@
             </form>
             
         </div>
-        
-    </div>
-    
-</div>
 
-<div class="col-1">&nbsp;</div>
-
-<div class="col-3 d-none d-xs-none d-sm-none d-md-block d-lg-block d-xl-block">
+<div class="col-4 d-none d-xs-none d-sm-none d-md-flex d-lg-flex d-xl-flex justify-content-end">
     
-    <a class="mx-3 my-1" href="{{ route('cart.index') }}"><img src="/img/cart.png" alt="map" />
+    <a class="my-auto" href="{{ route('cart.index') }}"><img src="/img/cart.png" alt="Cart" />
         @auth
         @if(isset(Auth::user()->cart->services))
-        <span class="badge badge-light">{{ count(Auth::user()->cart->services) }}</span>
+        <span class="badge badge-pill badge-light">{{ count(Auth::user()->cart->services) }}</span>
         @endif
         @endauth
     </a>
     
-    <div class="d-inline dropdown mt-1">
+    <div class="d-inline dropdown my-auto">
        
-        <a class="mx-3" href="#" data-toggle="dropdown" aria-haspopup="true" aria-label="User Parameters" id="userDropdown"><img src="/img/user.png" alt="map" /></a>
+        <a class="mx-3" href="#" data-toggle="dropdown" aria-haspopup="true" aria-label="User Parameters" id="userDropdown"><img src="/img/user.png" alt="User" /></a>
         <div class="dropdown-menu bg-success mt-5 mr-2" aria-labelledby="userDropdown">
         @if (Route::has('login'))
             @auth
@@ -98,7 +92,7 @@
         @endif
             
         </div>
-
+                
     </div>
-
+    
 </div>
