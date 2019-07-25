ca1577ff198edd0d81a356e1f7272514576dc433
Dmytro Sumtsov <sumtsow@gmail.com>
2019-07-25 12:35:25 +0300

    Update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
 
 <div class="row mt-5">
     <div class="col-1 border-right border-dark align-items-start d-none d-xs-none d-sm-none d-md-flex d-lg-flex d-xl-flex" style="min-height: 18em; min-width: 7em">
-        <h3 class="h3 text-uppercase font-weight-bold m-3" style="transform: rotate(270deg) translateX(-5em)">{{ __('app.categories') }}</h3>   
+        <h3 class="h3 text-uppercase font-weight-bold" style="transform: rotate(270deg) translateX(-5em)">{{ __('app.categories') }}</h3>   
     </div>
 
     <div class="col">
@@ -26,8 +26,8 @@
 </div>
 
 <div class="row mt-5">
-    <div class="col-1 border-right border-dark align-items-start d-none d-xs-none d-sm-none d-md-flex d-lg-flex d-xl-flex" style="min-height: 14em; min-width: 7em">
-        <h3 class="h3 text-uppercase font-weight-bold" style="transform: rotate(270deg) translateX(-3em);">{{ __('app.actions') }}</h3>   
+    <div class="col-1 border-right border-dark align-items-start d-none d-xs-none d-sm-none d-md-flex d-lg-flex d-xl-flex" style="min-height: 18em; min-width: 7em">
+        <h3 class="h3 text-uppercase font-weight-bold ml-4" style="transform: rotate(270deg) translateX(-3em);">{{ __('app.actions') }}</h3>   
     </div>
 
     <div class="col">
